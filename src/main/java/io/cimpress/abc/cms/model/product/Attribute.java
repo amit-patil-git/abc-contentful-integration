daf26046b706d71568952cc61341be539062703b
Amit Patil <ampatil@vistaprint.com>
2019-03-25 10:11:43 +0530

    attribute vaule in range bug solved

--- src/main/java/io/cimpress/abc/cms/model/product/Attribute.java
+++ src/main/java/io/cimpress/abc/cms/model/product/Attribute.java
@@ -1,11 +1,13 @@
 
 package io.cimpress.abc.cms.model.product;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonIgnoreProperties(ignoreUnknown = true)
 @JsonPropertyOrder({
     "name",
     "value",
--- src/main/java/io/cimpress/abc/cms/model/product/AttributeValueInRange.java
+++ src/main/java/io/cimpress/abc/cms/model/product/AttributeValueInRange.java
@@ -1,7 +1,10 @@
 package io.cimpress.abc.cms.model.product;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 import java.util.Set;
 
+@JsonIgnoreProperties(ignoreUnknown = true)
 public final class AttributeValueInRange implements AttributeValue {
 
 	private String type;
@@ -34,12 +37,14 @@ public final class AttributeValueInRange implements AttributeValue {
 	public void setRange(Set<Range> range) {
 		this.range = range;
 	}
-	
+
+	@JsonIgnoreProperties(ignoreUnknown = true)
 	public static class Range {
 		private float minimum;
 		private float maximum;
 		private float increment;
 		private String description;
+		private String unitOfMeasure;
 		
 		public float getMinimum() {
 			return minimum;
@@ -68,9 +73,17 @@ public final class AttributeValueInRange implements AttributeValue {
 		public String getDescription() {
 			return description;
 		}
-		
+
 		public void setDescription(String description) {
 			this.description = description;
+		}
+
+		public String getUnitOfMeasure() {
+			return unitOfMeasure;
+		}
+
+		public void setUnitOfMeasure(String unitOfMeasure) {
+			this.unitOfMeasure = unitOfMeasure;
 		}
 		
 	}
